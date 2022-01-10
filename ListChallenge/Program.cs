0119bff548cd54990e0f61c587499a815cf2c6d3
Henry Johnson <16JohnsH@fulford.york.sch.uk>
2022-01-10 13:38:31 +0000

    added list of scores fixes #2

--- ListChallenge/Program.cs
+++ ListChallenge/Program.cs
@@ -11,7 +11,10 @@ namespace ListChallenge
         static void Main(string[] args)
         {
             //List of names
-            List<string> names = new List<string> { "Will R", "Will D", "Ethan T", "Cormac R", "Adam B", "Asthon R", "Ben H", "Tom A", "Henry J"};            
+            List<string> names = new List<string> { "Will R", "Will D", "Ethan T", "Cormac R", "Adam B", "Asthon R", "Ben H", "Tom A", "Henry J"};
+
+            //List of scores
+            List<int> score = new List<int> { 40, 36, 47, 24, 42, 35, 37, 31, 28 };
 
             Console.WriteLine(names[3]);
         }
